fa20bc0f39dd348fd586578097c930b28df9e106
sunfl0w <lucaanthonyschwarz@googlemail.com>
2020-01-08 11:45:15 +0100

    Added more options.

--- CppBuildAid.py
+++ CppBuildAid.py
@@ -3,19 +3,18 @@ import argparse
 import subprocess
 import shutil
 import os
+from datetime import datetime
 
 print("Starting CppBuildAid")
+startTime = datetime.now()
 
 argumentParser = argparse.ArgumentParser()
-argumentParser.add_argument("-bt", "--buildType")
-argumentParser.add_argument("-bl", "--buildLanguage")
-argumentParser.add_argument("-nbt", "--numberOfBuildThreads")
+argumentParser.add_argument("-bt", "--buildType", default="debug")
+argumentParser.add_argument("-pt", "--publishingType", default="executable")
+argumentParser.add_argument("-bl", "--buildLanguage", default="C++")
+argumentParser.add_argument("-nbt", "--numberOfBuildThreads", default=1)
 
 args = argumentParser.parse_args()
-
-typeOfBuild = "debug"
-if(args.buildType is "release"):
-    typeOfBuild = "release"
 
 numberOfBuildThreads = 1
 if(args.numberOfBuildThreads is not None and args.numberOfBuildThreads >= 1):
@@ -28,7 +27,9 @@ if(os.path.exists("build")):
 print("Running cmake")
 
 subprocess.call(["cmake", "-H.", "-Bbuild"])
-subprocess.call(["cmake", "-DCMAKE_BUILD_TYPE={}".format(typeOfBuild.capitalize()), "."], cwd="build")
-subprocess.call(["cmake", "--build", ".", "--", "-j", str(numberOfBuildThreads)], cwd="build")
+subprocess.call(
+    ["cmake", "-DCMAKE_BUILD_TYPE={}".format(args.buildType.capitalize()), "."], cwd="build")
+subprocess.call(["cmake", "--build", ".", "--", "-j",
+                 str(numberOfBuildThreads)], cwd="build")
 
-print("Build completed successfully")
+print("Build completed successfully in {} ms.".format(datetime.now() - startTime))
