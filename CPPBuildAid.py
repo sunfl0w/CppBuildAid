1438e77fac51c97d1dee3d029f6b3e49d8a524d7
sunfl0w <lucaanthonyschwarz@googlemail.com>
2020-04-30 00:52:35 +0200

    Got a basic version of the script working.

---
+++ CPPBuildAid.py
@@ -0,0 +1,88 @@
+import sys
+import argparse
+import subprocess
+import shutil
+import os
+import glob
+from datetime import datetime
+
+
+def generateSourceList():
+    sourceFiles = []
+    sourcePath = os.path.join(os.getcwd(), "src")
+    if (sourcePath):
+        for dirpath, dirs, files in os.walk(sourcePath):
+            for filename in files:
+                if(str(filename).endswith('.c') or str(filename).endswith('.cpp')):
+                    relativeFilePath = os.path.relpath(os.path.join(dirpath, filename), os.getcwd())
+                    sourceFiles.append(relativeFilePath)
+        return sourceFiles
+    else:
+        print("Unable to find src directory. Terminating")
+        exit(1)
+
+
+def generateIncludeList():
+    headerFiles = []
+    includePath = os.path.join(os.getcwd(), "include")
+    if (includePath):
+        for dirpath, dirs, files in os.walk(includePath):
+            for filename in files:
+                if(str(filename).endswith('.h') or str(filename).endswith('.hpp')):
+                    relativeFilePath = os.path.relpath(os.path.join(dirpath, filename), os.getcwd())
+                    headerFiles.append(relativeFilePath)
+        return headerFiles
+    else:
+        print("Unable to find include directory. Terminating")
+        exit(1)
+
+
+def build(argv):
+    print("Starting CPPBuildAid")
+    startTime = datetime.now()
+
+    argumentParser = argparse.ArgumentParser()
+    argumentParser.add_argument("-bt", "--buildType")
+    argumentParser.add_argument("-t", "--threads")
+
+    args = argumentParser.parse_args()
+
+    typeOfBuild = "debug"
+    if(args.buildType == "release"):
+        typeOfBuild = "release"
+
+    threads = 1
+    if(args.threads is not None and args.threads >= 1):
+        threads = args.threads
+
+    relativeSourceFilePaths = generateSourceList()
+    sourceListFile = open("sourcelist.cmake", "w")
+    for relativeSourceFilePath in relativeSourceFilePaths:
+        sourceListFile.write(relativeSourceFilePath + "\n")
+    sourceListFile.close()
+
+    relativeHeaderFilePaths = generateIncludeList()
+    includeListFile = open("includelist.cmake", "w")
+    for relativeHeaderFilePath in relativeHeaderFilePaths:
+        includeListFile.write(relativeHeaderFilePath + "\n")
+    includeListFile.close()
+
+    print("Running cmake")
+    print("Using {} threads to build in {}-mode".format(threads, typeOfBuild))
+
+    subprocess.call(["cmake", "-S.", "-Bbuild"])
+    resultBuildType = subprocess.call(["cmake", "-DCMAKE_BUILD_TYPE={}".format(typeOfBuild.capitalize()), "."], cwd="build")
+    resultBuild = subprocess.call(["cmake", "--build", ".", "-j", str(threads)], cwd="build")
+
+    if(resultBuildType != 0 or resultBuild != 0):
+        print("Build failed with errors")
+        exit(1)
+    print("Build completed successfully in {} ms.".format(datetime.now() - startTime))
+
+
+def main():
+    build(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# CppBuildAid
+# CPPBuildAid
 
-This is a small script that helps with building C++ and C projects using cmake. I guess there are many such things, but this is mine. The script should be placed in the root directory of the project right next to the build directory.
+## Description:
+This is a small script that helps with building C++ and C projects using cmake. I guess there are many such things, but this is mine. The script should be placed in the root directory of the project right next to the src and include directories and the CMakeLists.txt. This script will create a includelist.cmake and a sourcelist.cmake with all source and header files to use in a CMakeLists.txt via:
 
+```
+file(STRINGS "includelist.cmake" includes)
+file(STRINGS "sourcelist.cmake" sources)
+
+include_directories(${includes})
+
+add_executable(<TEST> ${sources})
+```
+
+This makes building easier beacause no one has to keep track of those files, they are added to the list files recursively.
+
+## Options:
 Currently it supports these options:
 * -bt --buildType -> debug(default)/release
-* -nbt --numberOfBuildThreads -> 1(default)/any positive value except zero
+* -nbt --numberOfBuildThreads -> 1(default)/any unsigned integer except zero
